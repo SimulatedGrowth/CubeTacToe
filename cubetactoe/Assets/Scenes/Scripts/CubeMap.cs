6f6beffdf867259bc2000eb26b7e4f3c73db36e8
SimulatedGrowth <guvencdeniz.karaslan@hotmail.com>
2024-11-26 23:36:59 -0500

    All faces raycast and failed but implemented turning for sides

--- cubetactoe/Assets/Scenes/Scripts/CubeMap.cs
+++ cubetactoe/Assets/Scenes/Scripts/CubeMap.cs
@@ -30,41 +30,41 @@ public class CubeMap : MonoBehaviour
         cubeState = FindAnyObjectByType<CubeState>();
 
         UpdateMap(cubeState.front, front);
-        //UpdateMap(cubeState.back, back);
-        //UpdateMap(cubeState.left, left);
-        //UpdateMap(cubeState.right, right);
-        //UpdateMap(cubeState.up, up);
-        //UpdateMap(cubeState.down, down);
+        UpdateMap(cubeState.back, back);
+        UpdateMap(cubeState.left, left);
+        UpdateMap(cubeState.right, right);
+        UpdateMap(cubeState.up, up);
+        UpdateMap(cubeState.down, down);
     }
     void UpdateMap(List<GameObject> face, Transform side)
     {
         int i = 0;
         foreach (Transform map in side)
         {
-            if (face[0].name[0] == 'F')
+            if (face[i].name[0] == 'F')
             {
                 map.GetComponent<Image>().color = Color.red;
             }
-            //if (face[i].name[0] == 'B')
-            //{
-            //    map.GetComponent<Image>().color = new Color(1, 0.5f, 0, 1);
-            //}
-            //if (face[i].name[0] == 'U')
-            //{
-            //    map.GetComponent<Image>().color = Color.yellow;
-            //}
-            //if (face[i].name[0] == 'D')
-            //{
-            //    map.GetComponent<Image>().color = Color.white;
-            //}
-            //if (face[i].name[0] == 'L')
-            //{
-            //    map.GetComponent<Image>().color = Color.blue;
-            //}
-            //if (face[i].name[0] == 'R')
-            //{
-            //    map.GetComponent<Image>().color = Color.green;
-            //}
+            if (face[i].name[0] == 'B')
+            {
+                map.GetComponent<Image>().color = new Color(1, 0.5f, 0, 1);
+            }
+            if (face[i].name[0] == 'U')
+            {
+                map.GetComponent<Image>().color = Color.yellow;
+            }
+            if (face[i].name[0] == 'D')
+            {
+                map.GetComponent<Image>().color = Color.white;
+            }
+            if (face[i].name[0] == 'L')
+            {
+                map.GetComponent<Image>().color = Color.blue;
+            }
+            if (face[i].name[0] == 'R')
+            {
+                map.GetComponent<Image>().color = Color.green;
+            }
             i++;
         }
     }
--- cubetactoe/Assets/Scenes/Scripts/CubeState.cs
+++ cubetactoe/Assets/Scenes/Scripts/CubeState.cs
@@ -12,16 +12,40 @@ public class CubeState : MonoBehaviour
     public List<GameObject> down = new List<GameObject>();
     public List<GameObject> left = new List<GameObject>();
     public List<GameObject> right = new List<GameObject>();
-    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void PickUp(List<GameObject> cubeSide)
+    {
+        foreach (GameObject face in cubeSide)
+        {
+            if (face != cubeSide[4])
+            {
+                face.transform.parent.transform.parent = cubeSide[4].transform.parent;
+            }
+        }
+        //start the side rotation logic
+        cubeSide[4].transform.parent.GetComponent<PivotRotation>().Rotate(cubeSide);
+    }
+
+    public void PutDown(List<GameObject> littleCubes, Transform pivot)
+    {
+        foreach (GameObject littleCube in littleCubes)
+        {
+            if(littleCube != littleCubes[4])
+            {
+                littleCube.transform.parent.transform.parent = pivot;
+            }
+        }
     }
 }
